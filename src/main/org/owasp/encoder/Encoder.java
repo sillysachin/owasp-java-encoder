5b30628d4a75a14e9860813480d66666f80aac25
manico.james@gmail.com <manico.james@gmail.com@5d92c40a-c5ea-64f4-459b-e0b66671bd6c>
2011-03-01 02:25:57 +0000

    changing com to org

--- src/main/org/owasp/encoder/Encoder.java
+++ src/main/org/owasp/encoder/Encoder.java
@@ -32,7 +32,7 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
 // OF THE POSSIBILITY OF SUCH DAMAGE.
 
-package org.owasp.jxt;
+package org.owasp.encoder;
 
 import java.io.IOException;
 import java.io.PrintWriter;
